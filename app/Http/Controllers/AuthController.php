213e947ee72ef9e1d35f762d7892a4dcc791b633
LuisDonaldo2003 <l21930214@cdaltamirano.tecnm.mx>
2025-03-26 11:27:13 -0600

    MiddlewareUpdate

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -1,41 +1,77 @@
 <?php
-
+ 
 namespace App\Http\Controllers;
-
-use App\Http\Controllers\Controller;
+ 
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Routing\Controller;
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests; // Importar el trait necesario
 use App\Models\User;
-use Illuminate\Support\Facades\Validator;
-
-
+use Validator;
+ 
 class AuthController extends Controller
 {
-
+    use AuthorizesRequests; // Usar el trait para habilitar el método authorize()
+    
+    /**
+     * Create a new AuthController instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth:api', ['except' => ['login','register']]);
+    }
+ 
+ 
     /**
      * Register a User.
      *
      * @return \Illuminate\Http\JsonResponse
      */
     public function register() {
+
         $validator = Validator::make(request()->all(), [
             'name' => 'required',
             'email' => 'required|email|unique:users',
             'password' => 'required|min:8',
         ]);
-
+ 
         if($validator->fails()){
             return response()->json($validator->errors()->toJson(), 400);
         }
-
+ 
         $user = new User;
         $user->name = request()->name;
         $user->email = request()->email;
         $user->password = bcrypt(request()->password);
         $user->save();
-
+ 
         return response()->json($user, 201);
     }
+    
+    public function reg() {
 
+        $this->authorize('create',User::class);
 
+        $validator = Validator::make(request()->all(), [
+            'name' => 'required',
+            'email' => 'required|email|unique:users',
+            'password' => 'required|min:8',
+        ]);
+ 
+        if($validator->fails()){
+            return response()->json($validator->errors()->toJson(), 400);
+        }
+ 
+        $user = new User;
+        $user->name = request()->name;
+        $user->email = request()->email;
+        $user->password = bcrypt(request()->password);
+        $user->save();
+ 
+        return response()->json($user, 201);
+    }
+ 
     /**
      * Get a JWT via given credentials.
      *
@@ -48,10 +84,10 @@ class AuthController extends Controller
         if (! $token = auth('api')->attempt($credentials)) {
             return response()->json(['error' => 'Unauthorized'], 401);
         }
-
+ 
         return $this->respondWithToken($token);
     }
-
+ 
     /**
      * Get the authenticated User.
      *
@@ -61,7 +97,13 @@ class AuthController extends Controller
     {
         return response()->json(auth('api')->user());
     }
-
+    
+    function list() {
+        $users = User::all();
+        return response()->json([
+            "users" => $users,
+        ]);
+    }
     /**
      * Log the user out (Invalidate the token).
      *
@@ -69,11 +111,11 @@ class AuthController extends Controller
      */
     public function logout()
     {
-        auth('api')->logout();
-
+        auth()->logout();
+ 
         return response()->json(['message' => 'Successfully logged out']);
     }
-
+ 
     /**
      * Refresh a token.
      *
@@ -81,9 +123,9 @@ class AuthController extends Controller
      */
     public function refresh()
     {
-        return $this->respondWithToken(auth('api')->refresh());
+        return $this->respondWithToken(auth()->refresh());
     }
-
+ 
     /**
      * Get the token array structure.
      *
@@ -93,11 +135,21 @@ class AuthController extends Controller
      */
     protected function respondWithToken($token)
     {
+        $permissions = auth("api")->user()->getAllPermissions()->map(function($perm) {
+            return $perm->name;
+        });
         return response()->json([
             'access_token' => $token,
             'token_type' => 'bearer',
             'expires_in' => auth('api')->factory()->getTTL() * 60,
-            "user" => auth('api')->user(),
+            "user" => [
+                "name" => auth('api')->user()->name,
+                "surname" => auth('api')->user()->surname,
+                // "avatar" => auth('api')->user()->avartar,
+                "email"=> auth('api')->user()->email,
+                "roles" => auth('api')->user()->getRoleNames(),
+                "permissions" => $permissions,
+            ],
         ]);
     }
 }
