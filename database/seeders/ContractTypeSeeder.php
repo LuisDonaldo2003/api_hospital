f8173b5aa1152ba8c935b21a224491bc73ef905f
LuisDonaldo2003 <l21930214@cdaltamirano.tecnm.mx>
2025-04-09 11:15:44 -0600

    UpdateSeeders

--- database/seeders/ContractTypeSeeder.php
+++ database/seeders/ContractTypeSeeder.php
@@ -12,6 +12,20 @@ class ContractTypeSeeder extends Seeder
         $data = [
             ['name' => 'Sin cedula', 'state' => 1, 'created_at' => now(), 'updated_at' => now()],
             ['name' => 'Draft', 'state' => 1, 'created_at' => now(), 'updated_at' => now()],
+            ['name' => 'INSABI', 'state' => 1, 'created_at' => now(), 'updated_at' => now()],
+            ['name' => 'Homologado', 'state' => 1, 'created_at' => now(), 'updated_at' => now()],
+            ['name' => 'Guardias', 'state' => 1, 'created_at' => now(), 'updated_at' => now()],
+            ['name' => 'Regularizado', 'state' => 1, 'created_at' => now(), 'updated_at' => now()],
+            ['name' => 'IMSS Bienestar', 'state' => 1, 'created_at' => now(), 'updated_at' => now()],
+            ['name' => 'Formalizado 1', 'state' => 1, 'created_at' => now(), 'updated_at' => now()],
+            ['name' => 'Formalizado 2', 'state' => 1, 'created_at' => now(), 'updated_at' => now()],
+            ['name' => 'Formalizado 3', 'state' => 1, 'created_at' => now(), 'updated_at' => now()],
+            ['name' => 'Cuotas de Recuperación´', 'state' => 1, 'created_at' => now(), 'updated_at' => now()],
+            ['name' => 'Base', 'state' => 1, 'created_at' => now(), 'updated_at' => now()],
+            ['name' => 'Eventual', 'state' => 1, 'created_at' => now(), 'updated_at' => now()],
+            ['name' => 'Confianza', 'state' => 1, 'created_at' => now(), 'updated_at' => now()],
+
+
         ];
 
         DB::table('contract_types')->insert($data);
